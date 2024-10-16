b0e029a8e169b9e658d78ffbfc7830ce53407c26
Sungjin Lee <sodyn99@gmail.com>
2024-10-16 02:13:25 +0000

    Update to use kfp-server-api version 2
    - Fix cannot fetch experiments, runs information
    - Update test code for kfp-server-api v2.0.5
    - Change the '/runs' endpoint to retrieve pipeline information using 'pipeline_id'
    
    ISSUE-ID: AIMLFW-166
    
    Change-Id: I7949bef79a891764319f621189270cc59de03417
    Signed-off-by: Sungjin Lee <sodyn99@gmail.com>

--- kfadapter/kfadapter_main.py
+++ kfadapter/kfadapter_main.py
@@ -86,8 +86,8 @@ def get_experiment(expname):
         LOGGER.debug("Experiment name is present")
 
         LOGGER.debug(exp)
-        exp_dict['name'] = exp.name
-        exp_dict['id'] = exp.id
+        exp_dict['name'] = exp.display_name
+        exp_dict['id'] = exp.experiment_id
     except ValueError as err:
         LOGGER.error(err)
         raise BadRequest('Experiment name does not exist', status.HTTP_400_BAD_REQUEST,\
@@ -308,7 +308,7 @@ def list_pipelines():
         pipeline_list = KFCONNECT_KF_OBJ.get_kf_list_pipelines()
         pipe_dict['next_page_token'] = pipeline_list.next_page_token
         pipe_dict['total_size'] = pipeline_list.total_size
-        
+
         pipelines = []
         for pipeline in pipeline_list.pipelines:
             pipe_super_dict = {}
@@ -318,7 +318,7 @@ def list_pipelines():
             pipe_super_dict['created_at'] = pipeline.created_at
             pipelines.append(pipe_super_dict)
         pipe_dict['pipelines'] = pipelines
-        
+
     except:# pylint: disable=bare-except
         tbk = traceback.format_exc()
         LOGGER.error(tbk)
@@ -439,18 +439,22 @@ def list_runs():
         runs = KFCONNECT_KF_OBJ.get_kf_list_runs(KFCONNECT_CONFIG_OBJ.kf_dict['kfdefaultns'])
 
         for run in runs.runs:
+            LOGGER.debug(f"Run: {run}")
             run_super_dict = {}
-            run_super_dict['run_id'] = run.id
+            run_super_dict['run_id'] = run.run_id
             run_super_dict['run_description'] = run.description
-            run_super_dict['run_status'] = run.status
-            run_super_dict['experiment_name'] = run.resource_references[0].name
-            run_super_dict['experiment_id'] = run.resource_references[0].key.id
+            run_super_dict['run_status'] = run.state
+            run_super_dict['experiment_id'] = run.experiment_id
 
-            if len(run.resource_references) > 1:
-                run_super_dict['pipeline_name'] = run.resource_references[1].name
-                run_super_dict['pipeline_id'] = run.resource_references[1].key.id
+            LOGGER.debug(f"Pipeline version reference: {run.pipeline_version_reference}")
+            LOGGER.debug(f"Pipeline version reference ID: {run.pipeline_version_reference.pipeline_id}")
 
-            run_dict[run.name] = run_super_dict
+            if run.pipeline_version_reference is not None:
+                pipeline_info = KFCONNECT_KF_OBJ.get_kf_pipeline_desc(run.pipeline_version_reference.pipeline_id)
+                run_super_dict['pipeline_name'] = pipeline_info.display_name
+                run_super_dict['pipeline_id'] = pipeline_info.pipeline_id
+
+            run_dict[run.display_name] = run_super_dict
     except:# pylint: disable=bare-except
         tbk = traceback.format_exc()
         LOGGER.error(tbk)
